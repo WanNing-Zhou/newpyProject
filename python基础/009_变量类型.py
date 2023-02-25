735aaaad9fba531888b987111b008f2e229e3690
WangNing-Zhou <1879154660@qq.com>
2023-02-25 09:37:17 +0800

    python 基本数据类型

---
+++ python基础/009_变量类型.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :009_变量类型.py
+# @Time      :2023/2/25 9:18
+# @Author    :周万宁
+
+
+if __name__ == "__main__":
+    run_code = 0
+
+# 数据类型
+# Number 数值 : int float
+# boolean: 布尔
+# string 字符串
+# list 列表
+#  tuple 元组
+# dict 字典
+
+money = 5000  # int类型
+money1 = 2.33  # float类型
+
+# boolean 布尔
+# 流程控制语句
+
+# 字符串: 使用的是单引号 或者 双引号
+name = '勋悟空'
+name1 = '猪八戒'
+
+
+
+
+
+
