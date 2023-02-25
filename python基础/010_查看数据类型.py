7a3cec89e82cbe535323832dd85e552f74808462
WangNing-Zhou <1879154660@qq.com>
2023-02-25 11:26:54 +0800

    python 基本数据类型

---
+++ python基础/010_查看数据类型.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :010_查看数据类型.py
+# @Time      :2023/2/25 9:51
+# @Author    :周万宁
+
+
+if __name__ == "__main__":
+    run_code = 0
+
+name = '勋悟空'
+age = 12
+salary = 1000.21
+children = ['小红', '小白', '小红']
+child1 = {
+    'name': '小红',
+    'age': 16
+}
+
+print(type(name))
---
+++ python基础/011_类型转换.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :011_类型转换.py
+# @Time      :2023/2/25 10:14
+# @Author    :周万宁
+
+
+if __name__ == "__main__":
+    run_code = 0
+
+print('123.34转换成整型', int(123.34))  # 123
+print('123.34转换成字符串', str(123.34))  # '123.34'
+print('123.34转换bool值', bool(123.34))  # True
+print('123转换成浮点型', float(123))  # 123.0
