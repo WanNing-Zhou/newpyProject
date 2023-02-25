4ca9a3cd12f4bad2a54d83b1833b49a27b6e1461
WangNing-Zhou <1879154660@qq.com>
2023-02-25 16:05:16 +0800

    python if-else 语句的使用

---
+++ python基础/015_ 流程控制语句.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :015_ 流程控制语句.py
+# @Time      :2023/2/25 15:03
+# @Author    :周万宁
+
+
+if __name__ == "__main__":
+    run_code = 0
+
+age = input('请输入你的年龄:\n')
+age = int(age)
+if age >= 18:
+    print('您的年龄是%d,您已经成年,可以进入网吧' % age)
+elif age >= 17:
+    print('马上就快成年了,再等一等吧')
+elif age >= 13:
+    print('小朋友, 你的作业写完了吗')
+else:
+    print('未成年人不允许进入0网吧')
+
