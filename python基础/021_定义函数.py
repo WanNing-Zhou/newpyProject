9b577cb1dd115cef7e9768ade3512e0cf109af9b
WangNing-Zhou <1879154660@qq.com>
2023-02-26 09:33:24 +0800

    python 函数的使用

---
+++ python基础/021_定义函数.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :021_定义函数.py
+# @Time      :2023/2/26 8:57
+# @Author    :周万宁
+
+
+if __name__ == "__main__":
+    run_code = 0
+
+
+# 定义函数,能够完场打印信息功能
+def fun():
+    print('欢迎来到红浪漫')
+
+
+# 定义完函数后, 函数不是自动执行的, 需要调用它才可以
+fun()  # 欢迎来到红浪漫
+
+
+# 定义一个函数,进行两个数的加法运算
+def add(a, b):
+    c = a + b
+    print(c)
+
+
+# 调用函数
+add(1, 2)
+
+
+
+
+
+
