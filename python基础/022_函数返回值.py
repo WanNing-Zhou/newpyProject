13f4d99a60ed86fb809a175b282c1e8045d2536f
WangNing-Zhou <1879154660@qq.com>
2023-02-26 09:49:25 +0800

    python 函数的使用

---
+++ python基础/022_函数返回值.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :022_函数返回值.py
+# @Time      :2023/2/26 9:34
+# @Author    :周万宁
+
+
+if __name__ == "__main__":
+    run_code = 0
+
+
+# 定义一个函数计算两个数的加法
+def add(a, b):
+    c = a + b
+    return c
+
+
+print(add(1, 2))  # 3
---
+++ python基础/023_文件的打开与关闭.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :023_文件的打开与关闭.py
+# @Time      :2023/2/26 9:47
+# @Author    :周万宁
+
+
+if __name__ == "__main__":
+    run_code = 0
+
+
+
+
